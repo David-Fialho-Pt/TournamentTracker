7ba8d84d6852fa6c1b093362bc974d8be6cbedfe
David <davidfialho40@gmail.com>
2025-11-18 18:02:33 +0000

    Text Connection

--- TournamentTracker/TournamentTracker/DataAcess/TextConnector.cs
+++ TournamentTracker/TournamentTracker/DataAcess/TextConnector.cs
@@ -4,16 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAcess.TextHelpers;
 
 namespace TrackerLibrary.DataAcess
 {
     public class TextConnector : IDataConnection
     {
+
+        private const string PrizesFile = "PrizeModels.csv";
         // TODO - Wire up the CreatePrize for text files
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels().ToList();
+            int currentId = 1;
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentId;
+            prizes.Add(model);
+            prizes.SaveToPrizeFile(PrizesFile);
             return model;
+
         }
     }
 }
